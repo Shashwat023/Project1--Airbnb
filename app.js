37d094d06986f51742b5a203c258a69290c8fae2
Shashwat <avaiverma2004@gmail.com>
2025-06-30 18:12:56 +0530

    index route

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./model/listing.js");
+const Listing = require("./models/listing.js");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/WanderLust";
 
@@ -22,19 +26,24 @@ app.get("/", (req, res) => {
     res.send("I am Groot!");
 });
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "My New Villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India"
-    });
-
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing");
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("./listings/index.ejs", {allListings});
 });
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India"
+//     });
+
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing");
+// });
 
 app.listen(8080, () => {
     console.log("server is listening");
