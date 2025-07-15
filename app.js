d31da9c7594a21fd788641ffb9cb0bf020b23504
Shashwat <avaiverma2004@gmail.com>
2025-07-15 19:59:45 +0530

    client side & server side validation

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 app.set("view engine", "ejs");                    // |
 app.set("views", path.join(__dirname, "views"));  // | both for index route
@@ -44,41 +47,48 @@ app.get("/listings/new",  (req, res) => {
 });
 
 // show route 
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/show.ejs", {listing});
-});
+}));
 
 // Create route:- create - actual creation of instance
-app.post("/listings", async (req, res) => {
+app.post("/listings", wrapAsync(async (req, res, next) => {
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    if(result.error){
+        throw new ExpressError(400, result.error);
+    }
     let listing = req.body.listing;
     const newListing = new Listing(listing); // new Listing(listing) - instance
     await newListing.save();
     res.redirect("/listings");
-});
+})
+);
+
 
 // update route :- EDIT 
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/edit.ejs", {listing});
-});
+}));
 
 // update route :- update (put req)
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing}); // {...req.body.listing} - deconstructing app parameters and updating the values
     res.redirect("/listings");
-});
+}));
 
 // delete route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-});
+}));
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
@@ -94,6 +104,16 @@ app.delete("/listings/:id", async (req, res) => {
 //     res.send("successful testing");
 // });
 
+// app.all("*", (req, res, next) => {
+//     next(new ExpressError(404, "pg nt fnd!"));
+// });
+
+// middleware:-
+app.use((err, req, res, next) =>{
+    let{statusCode = 500, message = "something went wrong"} = err;
+    res.status(statusCode).render("error.ejs", {message});
+});
+
 app.listen(8080, () => {
     console.log("server is listening");
-});
+}); 
