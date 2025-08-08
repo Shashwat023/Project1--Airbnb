f7fe92599da13a9092304daeb3abf7bd3338e0a5
Shashwat <avaiverma2004@gmail.com>
2025-08-08 22:09:30 +0530

    all functionality for maps

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,6 @@
 const Listing = require("../models/listing");
+const geoToken = process.env.MAP_TOKEN;
+const geoapifyBaseUrl = `https://api.geoapify.com/v1/geocode/search?apiKey=${geoToken}`;
 
 module.exports.index = async (req, res) => {
     const allListings = await Listing.find({});
@@ -16,21 +18,31 @@ module.exports.showListing = async (req, res) => {
         req.flash("error", "Listing you requested for, does not exist!");
         return res.redirect("/listings");
     }
-    // console.log(listing);
-    res.render("./listings/show.ejs", {listing});
+
+    // Fetch geoapify data for this listing's location
+    const response = await fetch(`${geoapifyBaseUrl}&text=${encodeURIComponent(listing.location)}&limit=1`, { method: "GET" });
+    const data = await response.json();
+
+    res.render("./listings/show.ejs", {listing, data});
 };
 
 module.exports.createListing = async (req, res, next) => {
+    // to add map according to given location in newly added listing
+    const response = await fetch(`${geoapifyBaseUrl}&text=${encodeURIComponent(req.body.listing.location)}&limit=1`, { method: "GET" }); // response here is still a ReadableStream object from fetch — not the actual parsed JSON.
+    const data = await response.json(); // You must first convert it using .json()
+
     let url = req.file.path;
     let filename = req.file.filename;
-
     let listing = req.body.listing;
     const newListing = new Listing(listing); // new Listing(listing) - instance
     newListing.owner = req.user._id;
     newListing.image = {url, filename};
-    await newListing.save();
+    newListing.geometry = data.features[0].geometry;
+    let savedListing = await newListing.save();
+    console.log(savedListing);
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
+
 };
 
 module.exports.renderEditForm = async (req, res) => {
@@ -40,7 +52,9 @@ module.exports.renderEditForm = async (req, res) => {
         req.flash("error", "Listing you requested for, does not exist!");
         return res.redirect("/listings");
     }
-    res.render("./listings/edit.ejs", {listing});
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
+    res.render("./listings/edit.ejs", {listing, originalImageUrl});
 };
 
 module.exports.updateListing = async (req, res) => {
